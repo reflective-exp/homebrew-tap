570bc94b00fea031dc34a6cfed27c1baa5085912
Eric Saxby <77073+sax@users.noreply.github.com>
2026-09-11 09:24:45 -0500

    Update compostbin to v0.3.0

--- Formula/compostbin.rb
+++ Formula/compostbin.rb
@@ -2,11 +2,11 @@ class Compostbin < Formula
   desc "Compostbin"
   homepage "https://github.com/reflective-exp/compostbin"
   license "MIT"
-  version "0.2.0"
+  version "0.3.0"
 
   on_arm do
     url "https://github.com/reflective-exp/compostbin/releases/download/v#{version}/compostbin-darwin-arm64.tar.gz"
-    sha256 "48eb73add7d9e029693d41faf763969c362a831d8e03e3e668688aad19c4b59d"
+    sha256 "c2db955a3d550ffdd4a21ec611b8b815b8cc26ff35c84f5095a2cb4bc80f4906"
   end
 
   depends_on "container"
