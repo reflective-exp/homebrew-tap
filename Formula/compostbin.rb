d8f96baf2a0add5f4d0388b3e52b0541d7a7bac2
Eric Saxby <77073+sax@users.noreply.github.com>
2026-09-18 09:34:19 -0700

    Update compostbin to v0.8.0

--- Formula/compostbin.rb
+++ Formula/compostbin.rb
@@ -2,16 +2,27 @@ class Compostbin < Formula
   desc "Compostbin"
   homepage "https://github.com/reflective-exp/compostbin"
   license "MIT"
-  version "0.7.0"
+  version "0.8.0"
 
   on_arm do
     url "https://github.com/reflective-exp/compostbin/releases/download/v#{version}/compostbin-darwin-arm64.tar.gz"
-    sha256 "ea0815d3d90cc1a48f6a6354d007d9bfdcb76f89836e5fc0b98d1e690b56c48f"
+    sha256 "9b2cb79ecbca8684e542e67746f3bf6e08d05abf5796241a4998326801c2b4df"
   end
-
-  depends_on "container"
 
   def install
     bin.install "compostbin"
+    warn <<BREAKING
+
+    `compostbin` v0.8.0 no longer depends on Apple's `container` CLI, and requires
+    an image re-build before next use.
+
+    To clean up old images:
+
+        container stop -a
+        container prune
+        container image rm -a
+        container system stop
+
+BREAKING
   end
 end
